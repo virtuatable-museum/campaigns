5f71b00c59797ac7e2246747db5829f735769f82
Babausse <courtois.vincent@outlook.com>
2018-11-29 23:06:14 +0100

    Strengthen the test suite concerning dice rolls, and fix tests concerning files.

--- controllers/files.rb
+++ controllers/files.rb
@@ -1,19 +1,12 @@
 module Controllers
   class Files < Controllers::Base
 
-    attr_accessor :service
-
-    def initialize(argument)
-      super(argument)
-      @service = ::Services::Files.instance
-    end
-
     declare_route 'get', '/:id/files/:file_id' do
       _session = check_session('files_get')
       _campaign = get_campaign_for(_session, 'files_get', strict: false)
 
-      if service.campaign_has_file?(_campaign, params['file_id'])
-        service.get_campaign_file(_campaign, params['file_id'])
+      if ::Services::Files.instance.campaign_has_file?(_campaign, params['file_id'])
+        ::Services::Files.instance.get_campaign_file(_campaign, params['file_id'])
       else
         custom_error 404, 'files_get.file_id.unknown'
       end
@@ -22,7 +15,7 @@ module Controllers
     declare_route 'get', '/:id/files' do
       _session = check_session('files_list')
       _campaign = get_campaign_for(_session, 'files_list', strict: false)
-      halt 200, service.list(_campaign).to_json
+      halt 200, ::Services::Files.instance.list(_campaign).to_json
     end
 
     declare_route 'post', '/:id/files' do
@@ -30,10 +23,10 @@ module Controllers
       _session = check_session('files_creation')
       _campaign = get_campaign_for(_session, 'files_creation', strict: true)
 
-      file = service.create(_session, _campaign, params)
+      file = ::Services::Files.instance.create(_session, _campaign, params)
 
       if file.save
-        if service.store(file, params['content'])
+        if ::Services::Files.instance.store(file, params['content'])
           halt 200, Decorators::File.new(file).to_h.to_json
         else
           custom_error 400, 'files_creation.upload.failed'
@@ -47,8 +40,8 @@ module Controllers
       _session = check_session('files_deletion')
       _campaign = get_campaign_for(_session, 'files_deletion', strict: true)
 
-      if service.campaign_has_file?(_campaign, params['file_id'])
-        service.delete_campaign_file(_campaign, params['file_id'])
+      if ::Services::Files.instance.campaign_has_file?(_campaign, params['file_id'])
+        ::Services::Files.instance.delete_campaign_file(_campaign, params['file_id'])
         halt 200, {message: 'deleted'}.to_json
       else
         custom_error 404, 'files_deletion.file_id.unknown'
