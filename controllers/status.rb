e84ec2e2e9ef55867af537fb1d769c05836f1c21
babausse <courtois.vincent@outlook.com>
2019-08-06 12:51:42 -0700

    Add missing top-level documentation because I did'nt really run rubocop locally, ma bad

--- controllers/status.rb
+++ controllers/status.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Controllers
+  # Controller used to generate the status route used by the vigilante
+  # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Status < Arkaan::Utils::Controllers::Checked
     declare_status_route
   end
